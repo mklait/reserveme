60edb557ead296b68c176dc1e993761c1f5e50f9
Mariam Klait <mklait@gmail.com>
2017-10-22 18:22:02 -0400

    finished section 1. destroy not working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,20 +2,48 @@ class RestaurantsController < ApplicationController
 	def index
 		@restaurant = Restaurant.all
 	end 
-	
+
 	def show
 		@restaurant = Restaurant.find(params[:id])
 	end 
 
 	def new
+		@restaurant = Restaurant.new
 	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end 
 
 	def create
 		@restaurant = Restaurant.new(restaurant_params)
 
-		@restaurant.save
-		redirect_to @restaurant
+		if @restaurant.save
+	    	redirect_to @restaurant
+	  else
+	  	  render 'new'
 	end
+end
+
+def update
+	@restaurant = Restaurant.find(params[:id])
+
+	if @restaurant.update(restaurant_params)
+		redirect_to @restaurant
+	else
+		render 'edit'
+	end 
+end 
+
+def destroy
+	@restaurant.destroy
+	respond_to do |format|
+		format.html { redirect_to restaurants_url, notice: 'Restaurant was sucessfully Destroyed'}
+		format.json { head :no_content}
+
+	redirect_to restaurants_path
+end
+end
 
 private
 	def restaurant_params
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+	validates :name, presence: true,
+						length: {minimum: 3 }
+	validates :cuisine, presence: true,
+						length: {minimum: 3 }
+	validates :address, presence: true,
+						length: {minimum: 4 }
 end
